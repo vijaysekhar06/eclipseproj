85615adbdc7f0558c9e516e2127003c17da49a0f
swamy <palaganiayyappa@gmail.com>
2021-01-15 17:49:41 +0530

    sdasdadasda

--- src/main/java/com/javatechie/spring/eureaka/server/AmazonServerApplication.java
+++ src/main/java/com/javatechie/spring/eureaka/server/AmazonServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class AmazonServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("KKKKKKKKKKKK");
 		SpringApplication.run(AmazonServerApplication.class, args);
 	}
 }
